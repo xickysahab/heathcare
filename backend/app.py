bdc5f0dcb2d5e6d7041bfd914dcb6dd5a6eb36b1
Aagam <jainaagam908@gmail.com>
2026-05-05 14:12:23 +0530

    config file updated

--- backend/app.py
+++ backend/app.py
@@ -1,4 +1,5 @@
 from flask import Flask
+import os
 from config import Config
 from extensions import db, jwt
 from flask_cors import CORS
@@ -6,13 +7,20 @@ from flask_cors import CORS
 # Import Blueprints
 from routes.auth import auth_bp
 from routes.patient import patient_bp
-# (You can add doctor_bp later if created)
+
 
 def create_app():
     app = Flask(__name__)
 
     # Load Config
     app.config.from_object(Config)
+
+    #  ADD THIS PART (DATABASE_URL from Render)
+    DATABASE_URL = os.environ.get("DATABASE_URL")
+    if DATABASE_URL:
+        # Fix for Render (postgres → postgresql)
+        DATABASE_URL = DATABASE_URL.replace("postgres://", "postgresql://")
+        app.config["SQLALCHEMY_DATABASE_URL"] = DATABASE_URL
 
     # Initialize Extensions
     db.init_app(app)
@@ -23,10 +31,10 @@ def create_app():
     app.register_blueprint(auth_bp, url_prefix="/api/auth")
     app.register_blueprint(patient_bp, url_prefix="/api/patient")
 
-    # Default Route (for testing)
+    # Default Route
     @app.route("/")
     def home():
-        return {"message": "Healthcare API Running "}
+        return {"message": "Healthcare API Running"}
 
     return app
 
@@ -35,8 +43,7 @@ def create_app():
 if __name__ == "__main__":
     app = create_app()
 
-    # Create Tables Automatically
     with app.app_context():
-        db.create_all()
+        db.create_all()   # create tables (for testing)
 
-    app.run(debug=True)
+    app.run(host="0.0.0.0", port=5000, debug=True)
