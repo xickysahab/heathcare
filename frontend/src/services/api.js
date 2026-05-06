7c9bac5962c08f98491837f416b366394390fdd9
Aagam <jainaagam908@gmail.com>
2026-05-06 13:21:12 +0530

    fix api base url

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -1,7 +1,8 @@
 import axios from "axios";
 
 const API = axios.create({
-  baseURL: "http://127.0.0.1:5000/api",
+  // baseURL: "http://127.0.0.1:5000/api",
+  baseURL: "https://heathcare-p04x.onrender.com/api"
 });
 
 // Attach JWT token automatically
